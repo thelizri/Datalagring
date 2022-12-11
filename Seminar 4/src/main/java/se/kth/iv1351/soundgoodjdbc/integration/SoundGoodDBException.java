fefe115ee9313aae223f656317cee1c7c536e1a3
thelizri <karl.william.tony.carlstedt@gmail.com>
2022-12-11 22:31:29 +0100

    Exception

--- Seminar 4/src/main/java/se/kth/iv1351/soundgoodjdbc/integration/SoundGoodDBException.java
+++ Seminar 4/src/main/java/se/kth/iv1351/soundgoodjdbc/integration/SoundGoodDBException.java
@@ -1,10 +1,23 @@
 package main.java.se.kth.iv1351.soundgoodjdbc.integration;
 
-public class SoundGoodDBException extends Throwable {
-    public SoundGoodDBException(String failureMsg, Exception cause) {
-        
+public class SoundGoodDBException extends Exception {
+
+    /**
+     * Create a new instance thrown because of the specified reason and exception.
+     *
+     * @param failureMsg    Why the exception was thrown.
+     * @param cause The exception that caused this exception to be thrown.
+     */
+    public SoundGoodDBException(String failureMsg, Throwable cause) {
+        super(failureMsg, cause);
     }
 
+    /**
+     * Create a new instance thrown because of the specified reason.
+     *
+     * @param failureMsg Why the exception was thrown.
+     */
     public SoundGoodDBException(String failureMsg) {
+        super(failureMsg);
     }
 }
