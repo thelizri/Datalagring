4f4ba00cce7cd1d1e0a9e6b2b1b8a4603a390381
thelizri <karl.william.tony.carlstedt@gmail.com>
2022-12-11 22:48:23 +0100

    Controller

--- Seminar 4/src/main/java/se/kth/iv1351/soundgoodjdbc/controller/Controller.java
+++ Seminar 4/src/main/java/se/kth/iv1351/soundgoodjdbc/controller/Controller.java
@@ -1,4 +1,34 @@
 package main.java.se.kth.iv1351.soundgoodjdbc.controller;
 
+import main.java.se.kth.iv1351.soundgoodjdbc.integration.SoundGoodDAO;
+import main.java.se.kth.iv1351.soundgoodjdbc.integration.SoundGoodDBException;
+import main.java.se.kth.iv1351.soundgoodjdbc.model.InstrumentException;
+
+/**
+ * This is the application's only controller, all calls to the model pass here.
+ * The controller is also responsible for calling the DAO. Typically, the
+ * controller first calls the DAO to retrieve data (if needed), then operates on
+ * the data, and finally tells the DAO to store the updated data (if any).
+ */
 public class Controller {
+
+    private final SoundGoodDAO soundGoodDb;
+
+    /**
+     * Creates a new instance, and retrieves a connection to the database.
+     *
+     * @throws SoundGoodDBException If unable to connect to the database.
+     */
+    public Controller() throws SoundGoodDBException {
+        soundGoodDb = new SoundGoodDAO();
+    }
+
+    private void commitOngoingTransaction(String failureMsg) throws InstrumentException {
+        try {
+            soundGoodDb.commit();
+        } catch (SoundGoodDBException e) {
+            throw new InstrumentException(failureMsg, e);
+        }
+    }
+
 }
--- Seminar 4/src/main/java/se/kth/iv1351/soundgoodjdbc/model/InstrumentException.java
+++ Seminar 4/src/main/java/se/kth/iv1351/soundgoodjdbc/model/InstrumentException.java
@@ -1,5 +1,10 @@
 package main.java.se.kth.iv1351.soundgoodjdbc.model;
 
-public class InstrumentException {
+import main.java.se.kth.iv1351.soundgoodjdbc.integration.SoundGoodDBException;
 
+public class InstrumentException extends Exception {
+
+    public InstrumentException(String failureMsg, SoundGoodDBException e) {
+        super(failureMsg, e);
+    }
 }
