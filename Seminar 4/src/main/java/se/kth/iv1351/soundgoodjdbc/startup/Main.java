26d934e34aabfd89059a51f028e68257712a1680
thelizri <karl.william.tony.carlstedt@gmail.com>
2022-12-11 18:31:28 +0100

    Test

--- Seminar 4/src/main/java/se/kth/iv1351/soundgoodjdbc/startup/Main.java
+++ Seminar 4/src/main/java/se/kth/iv1351/soundgoodjdbc/startup/Main.java
@@ -1,7 +1,18 @@
 package main.java.se.kth.iv1351.soundgoodjdbc.startup;
 
+import java.sql.*;
+
+
 public class Main {
     public static void main(String[] args){
+        Connection connection = null;
 
+        try{
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/SoundGood","postgres","password");
+            if(connection != null) System.out.println("Connection ok");
+        }catch (Exception e){
+           e.printStackTrace();
+        }
     }
 }
