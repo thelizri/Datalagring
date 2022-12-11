320d563cef8ec3cf8f0eccee5d8069193aed8233
thelizri <karl.william.tony.carlstedt@gmail.com>
2022-12-11 19:31:42 +0100

    Added commmands

--- Seminar 4/src/main/java/se/kth/iv1351/soundgoodjdbc/view/Command.java
+++ Seminar 4/src/main/java/se/kth/iv1351/soundgoodjdbc/view/Command.java
@@ -1,4 +1,29 @@
 package main.java.se.kth.iv1351.soundgoodjdbc.view;
 
 public enum Command {
+    /**
+     * List all instruments of specified kind that are not rented.
+     */
+    LIST,
+    /**
+     * Command to rent an instrument. It is not possible for a student
+     * to rent more than two instruments.
+     */
+    RENT,
+    /**
+     * Command to terminate an ongoing rental of an instrument.
+     */
+    TERMINATE,
+    /**
+     * Get a list of commands.
+     */
+    HELP,
+    /**
+     * Leave the chat application.
+     */
+    QUIT,
+    /**
+     * None of the valid commands above was specified.
+     */
+    ILLEGAL_COMMAND
 }
